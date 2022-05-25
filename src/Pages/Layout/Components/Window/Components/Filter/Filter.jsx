075cb5e469f09af1b1f05b7b535f727081938d5b
Taguhi <taguhikarakash@gmail.com>
2022-05-25 17:50:34 +0200

    filter sucks

--- src/Pages/Layout/Components/Window/Components/Filter/Filter.jsx
+++ src/Pages/Layout/Components/Window/Components/Filter/Filter.jsx
@@ -1,8 +1,14 @@
 import Location from './Components/Location/Location';
+import Skills from './Components/Skills/Skills';
+import Promotions from './Components/Promotions/Promotions';
+import Availability from './Components/Availability/Availability';
 
 const Filter = () => {
   return <>
   <Location />
+  <Skills />
+  <Promotions /> 
+  <Availability /> 
   </>
 
 }
